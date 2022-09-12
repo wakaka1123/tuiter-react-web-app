d77ebf807a27e57cb3523fb7a5938b53e2d6af57
wakaka1123 <overmarsdyf@163.com>
2022-09-12 16:23:30 -0700

    added change to a2

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>
+          This is a branch for a2
+        </h2>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
