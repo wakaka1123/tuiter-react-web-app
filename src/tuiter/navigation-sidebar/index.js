1972ffa98b3949946b31bc42320364f7f5b38ff2
wakaka1123 <overmarsdyf@163.com>
2022-10-19 20:42:53 -0700

    added icons for left nav bar

--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -8,39 +8,39 @@ const NavigationSidebar = () => {
   const paths = pathname.split('/')
   const active = paths[2];
   return (   <div className="list-group">
-    <a href="/" className="list-group-item">Tuiter</a>
+    <a href="/" className="list-group-item"><i className="bi bi-twitter"></i> Tuiter</a>
     <Link to="/tuiter/home" className={`list-group-item ${active === 'home'?'active':''}`}>
-      Home
+      <i className="bi bi-house-door-fill"></i> Home
     </Link>
     <Link to="/tuiter/explore" className={`list-group-item ${active === 'explore'?'active':''}`}>
-      Explore
+      <i className="bi bi-airplane-fill"></i> Explore
     </Link>
     <Link to="/" className="list-group-item">
-      Labs
+      <i className="bi bi-tags-fill"></i> Labs
     </Link>
     <a href="/" className={`list-group-item
                     ${active === 'notifications'?'active':''}`}>
-      Notifications
+      <i className="bi bi-bell-fill"></i> Notifications
     </a>
     <a href="/" className={`list-group-item
                     ${active === 'messages'?'active':''}`}>
-      Messages
+      <i className="bi bi-chat-left-text-fill"></i> Messages
     </a>
     <a href="/" className={`list-group-item
                     ${active === 'bookmarks'?'active':''}`}>
-      Bookmarks
+      <i className="bi bi-bookmark-fill"></i> Bookmarks
     </a>
     <a href="/" className={`list-group-item
                     ${active === 'lists'?'active':''}`}>
-      Lists
+      <i className="bi bi-list-ul"></i> Lists
     </a>
     <a href="/" className={`list-group-item
                     ${active === 'profile'?'active':''}`}>
-      Profile
+      <i className="bi bi-person-circle"></i> Profile
     </a>
     <a href="/" className={`list-group-item
                     ${active === 'more'?'active':''}`}>
-      More
+      <i className="bi bi-three-dots"></i> More
     </a>
   </div>);
 };
